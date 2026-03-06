e55b730a4c3edd069e9af690d40fd05bf06d9474
HYKIM8282 <HEEYOUNGS9210@GMAIL.COM>
2026-03-06 16:43:58 +0900

    chore: apply pre-commit fixes

--- todo/admin.py
+++ todo/admin.py
@@ -1,1 +1,10 @@
 # Register your models here.
+# 가장 기본 등록 방식
+# Admin 옵션 커스터마이징 없음
+# 그냥 기본 관리자 화면으로 등록됨
+#
+#
+from django.contrib import admin
+from .models import Todo
+
+admin.site.register(Todo)
